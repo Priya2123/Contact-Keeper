3045faf84e41c0777d62044dbe698a9adc8d55dd
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-07-16 18:53:57 +0530

    token generation

--- config/db.js
+++ config/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
+      useUnifiedTopology: true,
     });
     console.log("MongoDB Connected");
   } catch (err) {
