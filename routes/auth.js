2e078f8334118a3de883f6d818089c560ecdfcc0
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-07-17 14:38:24 +0530

    auth middleware and protected routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,21 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth"); //for private routes
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
 
 //@route         GET api/auth
 //@description   Get logged in user
 //@access        Private
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    req.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Server Error");
+  }
 });
 
 //@route         POST api/auth
