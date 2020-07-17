403b543203b6fb66389cb97d53009d0b5ef19b96
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-07-17 13:18:37 +0530

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -34,6 +34,7 @@ router.post(
       if (!user) {
         return res.status(400).json({ msg: "Invalid Credentials" });
       }
+      //password matching
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
         return res.status(400).json({ msg: "Invalid Credentials" });
