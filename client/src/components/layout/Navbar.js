09db407d77b0b0c002feb4ac7193b903255acbf9
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-07-18 21:57:28 +0530

    Navbar and Route Setup

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </div>
+  );
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+Navbar.defaultProps = {
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt",
+};
+
+export default Navbar;
