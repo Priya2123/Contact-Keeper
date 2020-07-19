9c9eead40b94b04ebca8da19643881cd7946d0a2
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-07-19 14:14:49 +0530

    global state

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,3 +2,68 @@ import React, { useReducer } from "react";
 import uuid from "uuid";
 import ContactContext from "./ContactContext";
 import ContactReducer from "./ContactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+import contactContext from "./ContactContext";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Priya Aggarwal",
+        email: "aggarwalpriya1121@gmail.com",
+        phone: "1111111111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Molly Pie",
+        email: "mollypie@gmail.com",
+        phone: "2222222222",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "3333333333",
+        type: "professional",
+      },
+    ],
+  };
+  //dispatch: to dispatch objects to the reducer
+  const [state, dispatch] = useReducer(ContactReducer, initialState);
+
+  //ACTIONS
+  //Add Contact
+
+  //Delete Contact
+
+  //Set Current Contact
+
+  //Clear Current Contact
+
+  //Update Contact
+
+  //Filter Contacts
+
+  //Clear Filter
+
+  return (
+    <contactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}
+    >
+      {props.children}
+    </contactContext.Provider>
+  );
+};
