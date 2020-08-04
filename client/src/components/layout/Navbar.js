d5aa604e765d6d944de338b5e43bc805d3c904ca
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-08-04 17:05:44 +0530

    login navbar changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useContext, Fragment } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
 
 const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, logout, user } = authContext;
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a href="#!">
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+    </Fragment>
+  );
   return (
     <div className="navbar bg-primary">
       <h1>
